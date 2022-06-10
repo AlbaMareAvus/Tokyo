acab02e9f184888835ac752dedf97b47e74a5056
Vladimir <galkin.ve.home@gmail.com>
2022-06-10 14:36:56 +0300

    Добавлены функции для добавления данных о человеке в базу данных и получения количества строк в таблице staff

--- database/db_handler.py
+++ database/db_handler.py
@@ -17,3 +17,33 @@ def auth(login, password):
     cur.close()
     con.close()
     return result
+
+
+def add_person_to_database(first_name, second_name, third_name, post_name, file_path):
+    con = sqlite3.connect('database/face_recognition_db.db')
+    cur = con.cursor()
+
+    cur.execute(
+        """
+            INSERT INTO staff (first_name, second_name, third_name, post_name)
+            VALUES (?, ?, ?, ?, ?);
+        """, (first_name, second_name, third_name, post_name, file_path)
+    )
+    con.commit()
+
+    cur.close()
+    con.close()
+
+
+def get_count_of_staff():
+    con = sqlite3.connect('database/face_recognition_db.db')
+    cur = con.cursor()
+
+    cur.execute('SELECT COUNT(*) FROM staff;')
+    value = cur.fetchall()
+    result = value[0][0]
+
+    cur.close()
+    con.close()
+
+    return result
